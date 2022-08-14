de8d11fe3ac4ea61434798de769dc79baa238c33
Aneal Laryea <aneallaryea100@gmail.com>
2022-08-14 15:16:28 +0000

    fix linter

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
 
 module.exports = {
-    mode: 'development',
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
